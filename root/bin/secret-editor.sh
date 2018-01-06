c559715cb3ab0bc18ae21fd66a175d065271eb61
Emory Merryman <emory.merryman@gmail.com>
2018-01-06 14:32:37 +0000

    fix secret editor script

--- root/bin/secret-editor.sh
+++ root/bin/secret-editor.sh
@@ -28,6 +28,14 @@ CLOUD9_PORT=16845 &&
                 export HOST_PORT="${2}" &&
                     shift 2
             ;;
+            --read-write)
+                export READ_WRITE=$(uuidgen) &&
+                    shift
+            ;;
+            --read-only)
+                export READ_ONLY=$(uuidgen) &&
+                    shift
+            ;;
             *)
                 echo Unknown Option &&
                     echo ${0} &&
@@ -39,9 +47,8 @@ CLOUD9_PORT=16845 &&
     CIDFILE=$(mktemp) &&
     rm -f ${CIDFILE} &&
     cleanup(){
-        docker container stop $(cat ${CIDFILE}) &&
-            docker container rm --volumes $(cat ${CIDFILE}) &&
-            rm -f ${CIDFILE}
+        docker container stop $(cat ${CIDFILE}) && docker container rm --volumes $(cat ${CIDFILE})
+        rm -f ${CIDFILE}
     } &&
     trap cleanup EXIT &&
     docker \
@@ -52,13 +59,21 @@ CLOUD9_PORT=16845 &&
         --tty \
         --rm \
         --env PROJECT_NAME="${PROJECT_NAME}" \
-        --env CLOUD9_PORT="${CLOUD9_PORT}" \
+        --env CLOUD9_PORT=${CLOUD9_PORT} \
         --env GPG_SECRET_KEY="${GPG_SECRET_KEY}" \
         --env GPG2_SECRET_KEY="${GPG2_SECRET_KEY}" \
         --env GPG_OWNER_TRUST="${GPG_OWNER_TRUST}" \
         --env GPG2_OWNER_TRUST="${GPG2_OWNER_TRUST}" \
         --env GPG_KEY_ID="${GPG_KEY_ID}" \
         --env USER_EMAIL="${USER_EMAIL}" \
-        rebelplutonium/secret-editor:0.0.0 &&
-    docker network connect --alias ${EXTERNAL_NETWORK_NAME} $(cat ${CIDFILE}) &&
-    docker container start $(cat ${CIDFILE})
+        --env ORIGIN_ORGANIZATION="${ORIGIN_ORGANIZATION}" \
+        --env ORIGIN_REPOSITORY="${ORIGIN_REPOSITORY}" \
+        --env ORIGIN_ID_RSA="${ORIGIN_ID_RSA}" \
+        --env HOST_NAME="${HOST_NAME}" \
+        --env HOST_PORT="${HOST_PORT}" \
+        --env READ_WRITE="${READ_WRITE}" \
+        --env READ_ONLY="${READ_ONLY}"
+        --label expiry=${EXPIRY} \
+        rebelplutonium/secret-editor:1.0.0 &&
+    docker network connect --alias ${PROJECT_NAME} ${EXTERNAL_NETWORK_NAME} $(cat ${CIDFILE}) &&
+    docker container start --interactive $(cat ${CIDFILE})
