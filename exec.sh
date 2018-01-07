c7dea01c87d31dc63d483df840c899c9f547a33e
Emory Merryman <emory.merryman@gmail.com>
2018-01-07 21:47:35 +0000

    make independent

---
+++ exec.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+xhost +local: &&
+    TEMP_DIR=$(mktemp -d) &&
+    mkdir ${TEMP_DIR}/containers &&
+    mkdir ${TEMP_DIR}/networks &&
+    mkdir ${TEMP_DIR}/volumes &&
+    cleanup(){
+        ls -1 ${TEMP_DIR}/containers | while read FILE
+        do
+            sudo docker container stop $(cat ${TEMP_DIR}/containers/${FILE}) &&
+                sudo docker container rm --volumes $(cat ${TEMP_DIR}/containers/${FILE})
+        done &&
+        ls -1 ${TEMP_DIR}/networks | while read FILE
+        do
+            sudo docker network rm $(cat ${TEMP_DIR}/networks/${FILE})
+        done &&
+        ls -1 ${TEMP_DIR}/volumes | while read FILE
+        do
+            sudo docker volume rm $(cat ${TEMP_DIR}/volumes/${FILE})
+        done &&
+        rm -rf ${TEMP_DIR} &&
+        xhost
+    } &&
+    trap cleanup EXIT &&
+    while [ ${#} -gt 0 ]
+    do
+        case ${1} in
+            --hacker-version)
+                HACKER_VERSION="${2}" &&
+                    shift 2
+            ;;
+            --use-versioned-hacker-secrets)
+                USE_VERSIONED_HACKER_SECRETS=yes &&
+                    shift
+            ;;
+            *)
+                echo Unknown Option &&
+                    echo ${0} &&
+                    echo ${@} &&
+                    exit 64
+            ;;
+        esac
+    done &&
+    sudo docker volume create --label expiry=$(($(date +%s)+60*60*24*7)) > ${TEMP_DIR}/volumes/storage &&
+    sudo \
+        docker \
+        container \
+        create \
+        --cidfile ${TEMP_DIR}/containers/browser \
+        --mount type=bind,source=/tmp/.X11-unix/X0,destination=/tmp/.X11-unix/X0,readonly=true \
+        --mount type=volume,source=$(cat ${TEMP_DIR}/volumes/storage),destination=/srv/storage,readonly=false \
+        --label expiry=$(($(date +%s)+60*60*24*7)) \
+        rebelplutonium/browser:0.0.0 &&
+    export ORIGIN_ID_RSA="$(cat private/origin.id_rsa)" &&
+    export GPG_SECRET_KEY="$(cat private/gpg_secret_key)" &&
+    export GPG2_SECRET_KEY="$(cat private/gpg2_secret_key)" &&
+    export GPG_OWNER_TRUST="$(cat private/gpg_owner_trust)" &&
+    export GPG2_OWNER_TRUST="$(cat private/gpg2_owner_trust)" &&
+    sudo \
+        docker \
+        container \
+        create \
+        --interactive \
+        --tty \
+        --cidfile ${TEMP_DIR}/containers/hacker \
+        --env PROJECT_NAME="my-hacker" \
+        --env CLOUD9_PORT="10379" \
+        --env DISPLAY \
+        --env EXTERNAL_NETWORK_NAME \
+        --env USER_NAME="Emory Merryman" \
+        --env USER_EMAIL="emory.merryman@gmail.com" \
+        --env ORIGIN_ID_RSA \
+        --env GPG_SECRET_KEY \
+        --env GPG2_SECRET_KEY \
+        --env GPG_OWNER_TRUST \
+        --env GPG_KEY_ID=D65D3F8C \
+        --env SECRETS_ORIGIN_ORGANIZATION=nextmoose \
+        --env SECRETS_ORIGIN_REPOSITORY=secrets \
+        --privileged \
+        --mount type=bind,source=/tmp/.X11-unix/X0,destination=/tmp/.X11-unix/X0,readonly=true \
+        --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock,readonly=true \
+        --mount type=bind,source=/,destination=/srv/host,readonly=true \
+        --mount type=bind,source=/media,destination=/srv/media,readonly=false \
+        --mount type=bind,source=/home,destination=/srv/home,readonly=false \
+        --mount type=volume,source=$(cat ${TEMP_DIR}/volumes/storage),destination=/srv/storage,readonly=false \
+        --label expiry=$(($(date +%s)+60*60*24*7)) \
+        --entrypoint bash \
+        rebelplutonium/hacker:${HACKER_VERSION} &&
+    sudo docker network create --label expiry=$(($(date +%s)+60*60*24*7)) $(uuidgen) > ${TEMP_DIR}/networks/main &&
+    sudo docker network connect $(cat ${TEMP_DIR}/networks/main) $(cat ${TEMP_DIR}/containers/browser) &&
+    sudo docker network connect --alias hacker $(cat ${TEMP_DIR}/networks/main) $(cat ${TEMP_DIR}/containers/hacker) &&
+    sudo docker container start $(cat ${TEMP_DIR}/containers/browser) &&
+    sudo docker container start --interactive $(cat ${TEMP_DIR}/containers/hacker)
--- root/init.user.sh
+++ root/init.user.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 
-source ${HOME}/.bashrc &&
-    (cat <<EOF
-${AWS_ACCESS_KEY_ID}
-${AWS_SECRET_ACCESS_KEY}
-${AWS_DEFAULT_REGION}
-
-EOF
-    ) | aws configure &&
-    echo "${ORIGIN_ID_RSA}" > /home/user/.ssh/origin.id_rsa &&
-    echo "${UPSTREAM_ID_RSA}" > /home/user/.ssh/upstream.id_rsa &&
-    echo "${REPORT_ID_RSA}" > /home/user/.ssh/report.id_rsa &&
-    ln -sf /home/user/.ssh /opt/docker/workspace/dot_ssh &&
-    TEMP=$(mktemp -d) &&
+TEMP=$(mktemp -d) &&
     echo "${GPG_SECRET_KEY}" > ${TEMP}/gpg-secret-key &&
     gpg --batch --import ${TEMP}/gpg-secret-key &&
     echo "${GPG2_SECRET_KEY}" > ${TEMP}/gpg2-secret-key &&
@@ -30,6 +18,7 @@ EOF
     echo "${ORIGIN_ID_RSA}" > /home/user/.ssh/origin_id_rsa &&
     cat /opt/docker/extension/config >> /home/user/.ssh/config &&
     ln -sf /usr/bin/post-commit ${HOME}/.password-store/.git/hooks/post-commit &&
+    ln -sf /home/user/.ssh /opt/docker/workspace/dot_ssh &&
     ln -sf /home/user/bin /opt/docker/workspace &&
     ls -1 /usr/local/bin | while read FILE
     do
