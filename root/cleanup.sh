2055ea6f1923c3892e47e866363b4d4cbf3ec477
Emory Merryman <emory.merryman@gmail.com>
2018-01-13 19:33:06 +0000

    fix cleanup script

--- root/cleanup.sh
+++ root/cleanup.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
-ls -1 ${HOME}/ids/containers | while read FILE
+ls -1 /srv/ids/containers | while read FILE
 do
-    sudo /usr/bin/docker container stop $(cat ${HOME}/ids/containers/${FILE}) &&
-        sudo /usr/bin/docker container rm --volumes $(cat ${HOME}/ids/containers/${FILE})
+    sudo /usr/bin/docker container stop $(cat /srv/ids/containers/${FILE}) &&
+        sudo /usr/bin/docker container rm --volumes $(cat /srv/ids/containers/${FILE}) &&
+        rm -f /srv/ids/${FILE}
 done &&
-    ls -1 ${HOME}/ids/images | while read FILE
+    ls -1 /srv/ids/images | while read FILE
     do
-        sudo /usr/bin/docker image rm $(cat ${HOME}/ids/images/${FILE})
+        sudo /usr/bin/docker image rm $(cat /srv/ids/images/${FILE}) &&
+            rm -f /srv/ids/${FILE}
     done &&
-    ls -1 ${HOME}/ids/networks | while read FILE
+    ls -1 /srv/ids/networks | while read FILE
     do
-        sudo /usr/bin/docker network rm $(cat ${HOME}/ids/networks/${FILE})
+        sudo /usr/bin/docker network rm $(cat /srv/ids/networks/${FILE}) &&
+            rm -f /srv/ids/${FILE}
     done &&
-    ls -1 ${HOME}/ids/volumes | while read FILE
+    ls -1 /srv/ids/volumes | while read FILE
     do
-        sudo /usr/bin/docker volume rm $(cat ${HOME}/ids/volumes/${FILE})
-    done &&
-    rm -rf ${HOME}/ids
+        sudo /usr/bin/docker volume rm $(cat /srv/ids/volumes/${FILE}) &&
+            rm -f /srv/ids/${FILE}
+    done
         
