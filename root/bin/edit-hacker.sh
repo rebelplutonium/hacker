bee1491059694e83a5170b4fbba78b93fe648b02
Emory Merryman <emory.merryman@gmail.com>
2018-01-14 03:01:37 +0000

    launch git editor

---
+++ root/bin/edit-hacker.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+launch-git-editor -upstream-organization rebelplutonium --upstream-repository hacker --origin-organization nextmoose --origin-repository hacker --report-organization rebelplutonium --report-repository hacker --project-name bad2
