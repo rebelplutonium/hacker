a25c88e39457e313b2424b13062ea2dc3a614234
Emory Merryman <emory.merryman@gmail.com>
2018-01-14 02:01:20 +0000

    docker does not work as expected

--- root/bin/docker.sh
+++ root/bin/docker.sh
@@ -3,13 +3,4 @@
 sudo \
     --preserve-env \
     /usr/bin/docker \
-    container \
-    run \
-    --interactive \
-    --tty \
-    --rm \
-    --label expiry=$(($(date +%s)+60*60*24*7)) \
-    --volume /var/run/docker.sock:/var/run/docker.sock:ro \
-    --volume ${EXTERNAL_IDS_VOLUME}:/srv/ids \
-    docker:17.12.0 \
-    "${@}"
+    ${@}
