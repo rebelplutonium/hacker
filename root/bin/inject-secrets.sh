7365ee7e5d74d1b6642983258b0bd988a0bf86df
Emory Merryman <emory.merryman@gmail.com>
2018-01-14 19:36:57 -0500

    fix gnucash (#46)

--- root/bin/inject-secrets.sh
+++ root/bin/inject-secrets.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 pass show upstream.id_rsa > ${HOME}/.ssh/upstream.id_rsa &&
-    pass show report.id_rsa > ${HOME}/.ssh/report.id_rsa
+    pass show report.id_rsa > ${HOME}/.ssh/report.id_rsa &&
+    (
+        pass show aws-access-key-id &&
+            pass show aws-secret-access-key &&
+            echo us-east-1 &&
+            echo text
+    ) | aws configure
